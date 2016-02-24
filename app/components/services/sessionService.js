7fc55050b2894a1f218f7b16be11a38cdfbb594d
Andrey <ANLyhin@1cbit.ru>
2016-02-24 08:17:15 +0000

    new file:   app/components/services/sessionService.js

---
+++ app/components/services/sessionService.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* Services */
+
+/**
+ * Created by WebStorm.
+ * User: ANLyhin
+ * Date: 20.02.2016
+ * Time: 16:23
+ */
+
+var bitaskServices = angular.module('bitaskServices', ['ngResource']);
+
+bitaskServices.service('sessionService', function($stomp, $rootScope) {
+
+    // Connect to the server on path /sockjs and then create
+    // the STOMP protocol client
+    var socket = new SockJS('http://' + window.location.hostname + ':15674/stomp');
+    var stompClient = Stomp.over(socket);
+
+    stompClient.heartbeat.outgoing = 20000; // client will send heartbeats every 20000ms
+    stompClient.heartbeat.incoming = 0;
+
+    stompClient.connect('guest', 'guest',
+        function(frame) {
+            // receive notifications on the recog/sessions topic
+            stompClient.subscribe("/amq/queue/queue_name_from_server", function(message) {
+                $rootScope.$apply(function() {
+                    //$scope.sessions = angular.fromJson(message.body);
+                    $rootScope.sessions = message.body;
+                    //sessions = message.body;
+		    //console.log("scope.sessions: " + $scope.sessions);
+                });
+            });
+        },
+        function(error) {
+            console.log("STOMP protocol error " + error);
+        }
+    );
+
+    this.sendMessage = function(message) {
+	stompClient.send("/amq/queue/queue_name_to_server2", {"content-type":"text/plain"}, message);
+    };
+});
