3429b365b00939569afabe5e6bcf9cec6dd69d01
Andrey Lykhin <ANLyhin@1cbit.ru>
2016-03-28 17:44:02 +0700

    Сконвертировал глобальную дату на станице цели, в таймстамп.

--- app/views/goals/goals.js
+++ app/views/goals/goals.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/**
+ * Created by WebStorm.
+ * User: ANLyhin
+ * Date: 28.03.2016
+ * Time: 17:41
+ */
+
 angular.module('bitaskApp.goals', ['ngRoute', 'ngMaterial', 'bitaskApp.service.goal'])
 
 .config(['$routeProvider', '$mdIconProvider', function($routeProvider, $mdIconProvider) {
@@ -16,11 +23,14 @@ angular.module('bitaskApp.goals', ['ngRoute', 'ngMaterial', 'bitaskApp.service.g
 	//$scope.fromGoalDate = new Date();
 
 	/**
-	 * Изменилась дата
+	 * Изменилась дата глобального периода
 	 */
-	$scope.changeGoalDate = function() {
+	$scope.onChangeGlobalPeriod = function() {
 		console.log("change fromGoalDate: ", $scope.fromGoalDate);
+		console.log("timestamp fromGoalDate: ", new Date($scope.fromGoalDate).getTime()/1000);
+
 		console.log("change toGoalDate: ", $scope.toGoalDate);
+		console.log("timestamp toGoalDate: ", new Date($scope.toGoalDate).getTime()/1000);
 	};
 
 }]);
